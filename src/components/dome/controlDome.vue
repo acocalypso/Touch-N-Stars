2ed6ed09636a2a729f5701e59d2d5851045d7e0a
JohannesWorks <121811969+JohannesWorks@users.noreply.github.com>
2025-04-01 13:13:29 +0200

    add setDomeFollow

--- src/components/dome/controlDome.vue
+++ src/components/dome/controlDome.vue
@@ -6,12 +6,15 @@
   </div>
   <div class="flex gap-2 mt-2">
     <ButtonSlew />
+    
   </div>
+
 </template>
 
 <script setup>
 import ButtonOpenShutter from '@/components/dome/ButtonOpenShutter.vue';
 import ButtonStopShutter from '@/components/dome/ButtonStopShutter.vue';
 import ButtonCloseShutter from '@/components/dome/ButtonCloseShutter.vue';
-import ButtonSlew from '@/components/dome//ButtonSlewDome.vue';
+import ButtonSlew from '@/components/dome/ButtonSlewDome.vue';
+
 </script>

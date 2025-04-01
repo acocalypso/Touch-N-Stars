4e7836064fd8bdea85e92e3e0e456c19a27b80a0
JohannesWorks <121811969+JohannesWorks@users.noreply.github.com>
2025-04-01 08:49:39 +0200

    Dome button in own component

---
+++ src/components/dome/ButtonCloseShutter.vue
@@ -0,0 +1,22 @@
+<template>
+    <button @click="closeShutter" class="default-button-cyan">
+      {{ $t('components.dome.control.close') }}
+    </button>
+</template>
+
+<script setup>
+import apiService from '@/services/apiService';
+import { useI18n } from 'vue-i18n';
+
+const { t } = useI18n();
+
+async function closeShutter() {
+  try {
+    await apiService.domeAction('close');
+    console.log(t('components.dome.control.close'));
+  } catch (error) {
+    console.log(t('components.dome.control.errors.close'));
+  }
+}
+
+</script>

5fa7c48d0bb274ec41daf3a713175c04d1cd5324
JohannesWorks <121811969+JohannesWorks@users.noreply.github.com>
2025-04-02 20:00:32 +0200

    eslint 9

---
+++ eslint.config.js
@@ -0,0 +1,47 @@
+import { defineConfig, globalIgnores } from 'eslint/config';
+import vue from 'eslint-plugin-vue';
+import prettier from 'eslint-plugin-prettier';
+import vueParser from 'vue-eslint-parser';
+
+export default defineConfig([
+  globalIgnores([
+    '**/node_modules/**',
+    '**/dist/**',
+    '**/public/**',
+    '**/.github/**',
+    '**/android/**',
+    '**/Logo/**',
+    '**/resources/**',
+    '**/scripts/**',
+    'babel.config.js',
+    'eslint.config.js',
+    'postcss.config.js',
+  ]),
+
+  {
+    files: ['**/*.vue', '**/*.js'],
+    languageOptions: {
+      parser: vueParser,
+      parserOptions: {
+        parser: 'espree', 
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+      globals: {
+        defineProps: 'readonly',
+        defineEmits: 'readonly',
+        defineExpose: 'readonly',
+        withDefaults: 'readonly',
+      },
+    },
+    plugins: {
+      vue,
+      prettier,
+    },
+    rules: {
+      'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+      'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+      'prettier/prettier': ['error', { endOfLine: 'auto' }],
+    },
+  },
+]);

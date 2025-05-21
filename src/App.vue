c2258bf72623c48e92cdebfe609a31710f76c58e
JohannesWorks <121811969+JohannesWorks@users.noreply.github.com>
2025-05-21 15:25:15 +0200

    rework statusbar

--- src/App.vue
+++ src/App.vue
@@ -26,9 +26,6 @@
       <!-- Footer -->
       <div v-if="settingsStore.setupCompleted">
         <StatusBar class="fixed bottom-0 w-full" />
-        <!--  <button @click="showLogsModal = true" class="fixed bottom-0 w-full">
-          <LastMessage class="fixed bottom-0 w-full" />  
-        </button> -->
       </div>
     </div>
 

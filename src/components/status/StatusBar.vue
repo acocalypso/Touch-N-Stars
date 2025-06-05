9ea368577fdd556d7909e98b154b4971b9be72b4
JohannesWorks <121811969+JohannesWorks@users.noreply.github.com>
2025-06-05 11:01:28 +0200

    infobutton move to navbar

--- src/components/status/StatusBar.vue
+++ src/components/status/StatusBar.vue
@@ -195,24 +195,6 @@
         </svg>
       </button>
     </div>
-    <!-- About icon -->
-    <button
-      class="flex flex-row bg-cyan-950 p-1 shadow-lg rounded-full border border-cyan-800 gap-1"
-      @click.stop.prevent="handleAboutClick"
-    >
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        class="h-5 w-5"
-        viewBox="0 0 20 20"
-        fill="currentColor"
-      >
-        <path
-          fill-rule="evenodd"
-          d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z"
-          clip-rule="evenodd"
-        />
-      </svg>
-    </button>
 
     <!-- Weather modal -->
     <WeatherModal
@@ -220,8 +202,7 @@
       :weatherInfo="store.weatherInfo"
       @close="showWeatherModal = false"
     />
-    <!-- About modal -->
-    <AboutModal v-if="showAboutModal" :version="appVersion" @close="showAboutModal = false" />
+
     <!-- Log modal -->
     <LogModal v-if="showLogModal" @close="showLogModal = false" />
     <div
@@ -241,7 +222,6 @@ import { apiStore } from '@/store/store';
 import { ref } from 'vue';
 import { CameraIcon } from '@heroicons/vue/24/outline';
 import WeatherModal from '../WeatherModal.vue';
-import AboutModal from './AboutModal.vue';
 import LogModal from './LogModal.vue';
 import version from '@/version';
 import GuiderGraph from '../guider/GuiderGraph.vue';
@@ -260,11 +240,7 @@ function handleWeatherClick(event) {
   event.stopPropagation();
   event.preventDefault();
 }
-function handleAboutClick(event) {
-  showAboutModal.value = true;
-  event.stopPropagation();
-  event.preventDefault();
-}
+
 function handleLogClick(event) {
   showLogModal.value = true;
   event.stopPropagation();

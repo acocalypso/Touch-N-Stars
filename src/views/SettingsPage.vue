722bfb80f37c4387cde9a111bb98c104b78ae729
Johannes <info@touch-n-stars.eu>
2025-07-26 18:13:10 +0200

    setting are now an page

--- src/views/SettingsPage.vue
+++ src/views/SettingsPage.vue
@@ -1,9 +1,16 @@
 <template>
   <div class="min-h-screen bg-gray-900">
+    <!-- Titel -->
+    <div class="text-left mb-2">
+      <h1 class="text-xl text-center font-bold">{{ $t('components.settings.title') }}</h1>
+    </div>
     <SettingsComp />
   </div>
 </template>
 
 <script setup>
 import SettingsComp from '@/components/SettingsComp.vue';
+import { useI18n } from 'vue-i18n';
+
+const { t } = useI18n();
 </script>

820d4a6cd28c2ef5ea0c7a9b5aa5ea46806bcb23
Johannes <info@touch-n-stars.eu>
2025-08-01 21:22:14 +0200

    fix Slew and center

--- src/components/mount/ButtonSlewCenterRotate.vue
+++ src/components/mount/ButtonSlewCenterRotate.vue
@@ -104,13 +104,16 @@ async function slew() {
     centeringModalRef.value?.openModal();
     console.log('First: Slew to the target');
     await framingStore.slewAndCenterRotate(props.raAngle, props.decAngle, false, false);
-    console.log('First debug: Slew to the target', framingStore.slewIsStopt);
     if (framingStore.slewIsStopt) return;
     console.log('Second: center the target');
     await framingStore.slewAndCenterRotate(props.raAngle, props.decAngle, center, rotate);
+    
   }
 
-  await framingStore.slewAndCenterRotate(props.raAngle, props.decAngle, center, rotate);
+  if (rotate && store.rotatorInfo.Connected) {
+    console.log('Third: Rotate the mount');
+    await framingStore.slewAndCenterRotate(props.raAngle, props.decAngle, center, rotate);
+  }
   emit('finished'); // Emit Event nach Erfolg
 }
 </script>

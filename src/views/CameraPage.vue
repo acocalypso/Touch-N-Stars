a1b7d078b3e65ebb39240ef3aa4afd7f3a72a612
Johannes <Johannes_Maier@gmx.de>
2025-08-23 19:45:09 +0200

    add rotator to camerapage

--- src/views/CameraPage.vue
+++ src/views/CameraPage.vue
@@ -178,6 +178,22 @@
           </svg>
         </button>
       </div>
+      <div v-if="store.rotatorInfo.Connected">
+        <button
+          @click="openModal('rotator')"
+          :class="[buttonClasses, { 'glow-green': showRotator }]"
+          title="Rotator Controls"
+        >
+          <svg
+            :class="iconClasses"
+            viewBox="0 0 16 16"
+            fill="currentColor"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M6 7L7 6L4.70711 3.70711L5.19868 3.21553C5.97697 2.43724 7.03256 2 8.13323 2C11.361 2 14 4.68015 14 7.93274C14 11.2589 11.3013 14 8 14C6.46292 14 4.92913 13.4144 3.75736 12.2426L2.34315 13.6569C3.90505 15.2188 5.95417 16 8 16C12.4307 16 16 12.3385 16 7.93274C16 3.60052 12.4903 0 8.13323 0C6.50213 0 4.93783 0.647954 3.78447 1.80132L3.29289 2.29289L1 0L0 1V7H6Z"/>
+          </svg>
+        </button>
+      </div>
     </div>
 
     <ModalTransparanet :show="showMount" @close="showMount = false">
@@ -268,6 +284,36 @@
         </div>
       </template>
     </ModalTransparanet>
+
+    <ModalTransparanet :show="showRotator" @close="showRotator = false">
+      <template #header>
+        <div class="flex items-center justify-between w-full">
+          <h2 class="text-1xl font-semibold">{{ $t('components.rotator.title') }}</h2>
+          <div class="flex items-center gap-2 text-gray-400">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="w-4 h-4"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M8.25 15L12 18.75 15.75 15m-7.5-6L12 5.25 15.75 9"
+              />
+            </svg>
+            <span class="text-xs">Drag</span>
+          </div>
+        </div>
+      </template>
+      <template #body>
+        <div>
+          <controlRotator />
+        </div>
+      </template>
+    </ModalTransparanet>
   </div>
 </template>
 
@@ -287,6 +333,7 @@ import moveAxis from '@/components/mount/moveAxis.vue';
 import MoveFocuser from '@/components/focuser/MoveFocuser.vue';
 import ButtonsFastChangePositon from '@/components/focuser/ButtonsFastChangePositon.vue';
 import changeFilter from '@/components/filterwheel/changeFilter.vue';
+import controlRotator from '@/components/rotator/controlRotator.vue';
 import { downloadImage as downloadImageHelper } from '@/utils/imageDownloader';
 import apiService from '@/services/apiService';
 
@@ -300,6 +347,7 @@ const showModal = ref(false);
 const showMount = ref(false);
 const showFocuser = ref(false);
 const showFilter = ref(false);
+const showRotator = ref(false);
 
 // Modal Management - togglet das Modal oder schließt andere
 const openModal = (modalType) => {
@@ -310,6 +358,7 @@ const openModal = (modalType) => {
       } else {
         showFocuser.value = false;
         showFilter.value = false;
+        showRotator.value = false;
         showMount.value = true;
       }
       break;
@@ -319,6 +368,7 @@ const openModal = (modalType) => {
       } else {
         showMount.value = false;
         showFilter.value = false;
+        showRotator.value = false;
         showFocuser.value = true;
       }
       break;
@@ -328,7 +378,18 @@ const openModal = (modalType) => {
       } else {
         showMount.value = false;
         showFocuser.value = false;
+        showRotator.value = false;
         showFilter.value = true;
+      }
+      break;
+    case 'rotator':
+      if (showRotator.value) {
+        showRotator.value = false;
+      } else {
+        showMount.value = false;
+        showFocuser.value = false;
+        showFilter.value = false;
+        showRotator.value = true;
       }
       break;
   }

66f2dba74215199a0cecd37ed1b492968caba2de
Johannes <Johannes_Maier@gmx.de>
2025-08-30 20:13:06 +0200

    adjustments

--- src/store/store.js
+++ src/store/store.js
@@ -155,6 +155,7 @@ export const apiStore = defineStore('store', {
             () => apiService.fetchApiVersion(),
             this.connectingAttempts
           );
+          console.log('API Version response:', responseApiVersion);
           if (!responseApiVersion) {
             console.warn('API-Plugin not reachable');
             if (!this.errorMessageShown) {

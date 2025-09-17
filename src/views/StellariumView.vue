2a8ba11b157bd56a97b5bccd05ea9642982ab1f9
Johannes <Johannes_Maier@gmx.de>
2025-09-17 20:18:02 +0200

    format

--- src/views/StellariumView.vue
+++ src/views/StellariumView.vue
@@ -54,8 +54,19 @@
         @click="refreshStellarium"
         class="p-2 bg-gray-700 border border-cyan-600 rounded-full shadow-md z-10"
       >
-        <svg xmlns="http://www.w3.org/2000/svg" class="w-7 h-7 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15" />
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="w-7 h-7 text-white"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15"
+          />
         </svg>
       </button>
 

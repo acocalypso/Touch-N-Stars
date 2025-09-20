b0d91f70432c98791512f6c00a5526f3e872a387
Johannes <Johannes_Maier@gmx.de>
2025-09-20 10:29:32 +0200

    add to monitor

--- src/components/sequence/MonitorViewSetting.vue
+++ src/components/sequence/MonitorViewSetting.vue
@@ -64,6 +64,23 @@
       </div>
     </div>
 
+        <div
+      class="flex flex-row items-center justify-between w-full border border-gray-500 p-2 rounded-lg"
+    >
+      <label for="showSequenceCurrentState" class="text-gray-400">
+        {{ $t('components.sequence.monitor.settings.sequenceCurrentState') }}
+      </label>
+      <div>
+        <toggleButton
+          @click="
+            settingsStore.monitorViewSetting.showSequenceCurrentState =
+              !settingsStore.monitorViewSetting.showSequenceCurrentState
+          "
+          :status-value="settingsStore.monitorViewSetting.showSequenceCurrentState"
+        />
+      </div>
+    </div>
+
     <div
       class="flex flex-row items-center justify-between w-full border border-gray-500 p-2 rounded-lg"
     >

c352d977e12d1b821b189c942f71a689fd865375
Johannes <Johannes_Maier@gmx.de>
2025-11-18 14:16:18 +0100

    fix camera settings layout

--- src/components/camera/setSaveSnapshot.vue
+++ src/components/camera/setSaveSnapshot.vue
@@ -1,26 +1,26 @@
 <template>
-  <div class="flex flex-col w-full border border-gray-500 p-2 rounded-lg">
+  <div class="flex flex-col w-full border border-gray-500 p-1 md:p-2 rounded-lg">
     <div class="flex flex-row items-center justify-between">
-      <label for="toggle_light" class="text-gray-200">
+      <label for="toggle_light" class="text-xs md:text-sm text-gray-200">
         {{ $t('components.camera.set_save_snapshot') }}
       </label>
       <div>
         <toggleButton
           @click="toggleSave"
           :status-value="store.profileInfo.SnapShotControlSettings.Save"
-          class="pr-5 pl-5 justify-center"
+          class="pr-5 pl-5 justify-center h-7 md:h-8"
         />
       </div>
     </div>
-    <div class="flex flex-row items-center justify-between mt-3">
-      <label for="targetName" class="text-gray-200 mr-2">
+    <div class="flex flex-row items-center justify-between mt-2 md:mt-3">
+      <label for="targetName" class="text-xs md:text-sm text-gray-200 mr-2">
         {{ $t('components.camera.snapshotTargetName') }}
       </label>
       <input
         id="targetName"
         v-model="settingsStore.camera.snapshotTargetName"
         type="text"
-        class="default-input h-8 flex-1"
+        class="default-input h-7 md:h-8 flex-1 text-xs md:text-sm"
         :disabled="!store.profileInfo.SnapShotControlSettings.Save"
       />
     </div>

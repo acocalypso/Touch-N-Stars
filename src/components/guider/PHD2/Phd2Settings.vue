d17ce82003de49750a0d0e402c00273fa1dee4ed
Johannes <Johannes_Maier@gmx.de>
2025-12-19 10:50:00 +0100

    format

--- src/components/guider/PHD2/Phd2Settings.vue
+++ src/components/guider/PHD2/Phd2Settings.vue
@@ -10,113 +10,113 @@
       <PHD2Profil />
     </div>
 
-      <!-- Shared Parameters (always visible) -->
+    <!-- Shared Parameters (always visible) -->
+    <div
+      v-if="store.isPINS"
+      class="p-2 sm:p-4 flex flex-col gap-2 sm:gap-3 bg-gray-800/50 rounded-lg border border-gray-700/50"
+    >
+      <h3 class="font-bold text-base text-cyan-400">
+        {{ $t('components.guider.phd2.shared_prarmeters') }}
+      </h3>
+      <Phd2ReverseDecAfterFlip />
+    </div>
+
+    <template v-if="guiderStore.phd2IsConnected">
+      <!-- Calibration Settings -->
       <div
-        v-if="store.isPINS"
         class="p-2 sm:p-4 flex flex-col gap-2 sm:gap-3 bg-gray-800/50 rounded-lg border border-gray-700/50"
       >
         <h3 class="font-bold text-base text-cyan-400">
-          {{ $t('components.guider.phd2.shared_prarmeters') }}
+          {{ $t('components.guider.phd2.calibration') }}
         </h3>
-        <Phd2ReverseDecAfterFlip />
+        <div class="flex items-center justify-between">
+          <span class="text-sm font-medium text-gray-300">
+            {{ $t('components.guider.phd2.forceCalibration') }}
+          </span>
+          <toggleButton
+            @click="
+              settingsStore.setPhd2ForceCalibration(!settingsStore.guider.phd2ForceCalibration)
+            "
+            :status-value="settingsStore.guider.phd2ForceCalibration"
+          />
+        </div>
       </div>
 
-      <template v-if="guiderStore.phd2IsConnected">
-        <!-- Calibration Settings -->
-        <div
-          class="p-2 sm:p-4 flex flex-col gap-2 sm:gap-3 bg-gray-800/50 rounded-lg border border-gray-700/50"
-        >
-          <h3 class="font-bold text-base text-cyan-400">
-            {{ $t('components.guider.phd2.calibration') }}
-          </h3>
-          <div class="flex items-center justify-between">
-            <span class="text-sm font-medium text-gray-300">
-              {{ $t('components.guider.phd2.forceCalibration') }}
-            </span>
-            <toggleButton
-              @click="
-                settingsStore.setPhd2ForceCalibration(!settingsStore.guider.phd2ForceCalibration)
-              "
-              :status-value="settingsStore.guider.phd2ForceCalibration"
-            />
-          </div>
-        </div>
+      <!-- Exposure Settings -->
+      <div
+        class="p-2 sm:p-4 flex flex-col gap-2 sm:gap-3 bg-gray-800/50 rounded-lg border border-gray-700/50"
+      >
+        <h3 class="font-bold text-base text-cyan-400">
+          {{ $t('components.guider.phd2.exposure') }}
+        </h3>
+        <SetExposure />
+      </div>
 
-        <!-- Exposure Settings -->
-        <div
-          class="p-2 sm:p-4 flex flex-col gap-2 sm:gap-3 bg-gray-800/50 rounded-lg border border-gray-700/50"
-        >
-          <h3 class="font-bold text-base text-cyan-400">
-            {{ $t('components.guider.phd2.exposure') }}
-          </h3>
-          <SetExposure />
-        </div>
+      <!-- RA Algorithm -->
+      <div
+        class="p-2 sm:p-4 flex flex-col gap-2 sm:gap-3 bg-gray-800/50 rounded-lg border border-gray-700/50"
+      >
+        <h3 class="font-bold text-base text-cyan-400">
+          {{ $t('components.guider.phd2.raAlgorithm') }}
+        </h3>
+        <SetRaAlgoPara />
+      </div>
 
-        <!-- RA Algorithm -->
-        <div
-          class="p-2 sm:p-4 flex flex-col gap-2 sm:gap-3 bg-gray-800/50 rounded-lg border border-gray-700/50"
-        >
-          <h3 class="font-bold text-base text-cyan-400">
-            {{ $t('components.guider.phd2.raAlgorithm') }}
-          </h3>
-          <SetRaAlgoPara />
-        </div>
+      <!-- DEC Algorithm -->
+      <div
+        class="p-2 sm:p-4 flex flex-col gap-2 sm:gap-3 bg-gray-800/50 rounded-lg border border-gray-700/50"
+      >
+        <h3 class="font-bold text-base text-cyan-400">
+          {{ $t('components.guider.phd2.decAlgorithm') }}
+        </h3>
+        <SetDecAlgoPara />
+      </div>
+    </template>
 
-        <!-- DEC Algorithm -->
-        <div
-          class="p-2 sm:p-4 flex flex-col gap-2 sm:gap-3 bg-gray-800/50 rounded-lg border border-gray-700/50"
-        >
-          <h3 class="font-bold text-base text-cyan-400">
-            {{ $t('components.guider.phd2.decAlgorithm') }}
-          </h3>
-          <SetDecAlgoPara />
-        </div>
-      </template>
+    <template v-else-if="store.isPINS">
+      <!-- Equipment Settings -->
+      <div
+        class="p-2 sm:p-4 flex flex-col gap-2 sm:gap-3 bg-gray-800/50 rounded-lg border border-gray-700/50"
+      >
+        <h3 class="font-bold text-base text-cyan-400">
+          {{ $t('components.guider.phd2.equipment') }}
+        </h3>
+        <Phd2SelectCamera />
+        <Phd2SelectMount />
+      </div>
 
-      <template v-else-if="store.isPINS">
-        <!-- Equipment Settings -->
-        <div
-          class="p-2 sm:p-4 flex flex-col gap-2 sm:gap-3 bg-gray-800/50 rounded-lg border border-gray-700/50"
-        >
-          <h3 class="font-bold text-base text-cyan-400">
-            {{ $t('components.guider.phd2.equipment') }}
-          </h3>
-          <Phd2SelectCamera />
-          <Phd2SelectMount />
-        </div>
+      <!-- Optical Settings -->
+      <div
+        class="p-2 sm:p-4 flex flex-col gap-2 sm:gap-3 bg-gray-800/50 rounded-lg border border-gray-700/50"
+      >
+        <h3 class="font-bold text-base text-cyan-400">
+          {{ $t('components.guider.phd2.optical') }}
+        </h3>
+        <Phd2FocalLength />
+      </div>
 
-        <!-- Optical Settings -->
-        <div
-          class="p-2 sm:p-4 flex flex-col gap-2 sm:gap-3 bg-gray-800/50 rounded-lg border border-gray-700/50"
-        >
-          <h3 class="font-bold text-base text-cyan-400">
-            {{ $t('components.guider.phd2.optical') }}
-          </h3>
-          <Phd2FocalLength />
-        </div>
+      <!-- Calibration Settings -->
+      <div
+        class="p-2 sm:p-4 flex flex-col gap-2 sm:gap-3 bg-gray-800/50 rounded-lg border border-gray-700/50"
+      >
+        <h3 class="font-bold text-base text-cyan-400">
+          {{ $t('components.guider.phd2.calibration') }}
+        </h3>
+        <Phd2CalibrationStep />
+      </div>
 
-        <!-- Calibration Settings -->
-        <div
-          class="p-2 sm:p-4 flex flex-col gap-2 sm:gap-3 bg-gray-800/50 rounded-lg border border-gray-700/50"
-        >
-          <h3 class="font-bold text-base text-cyan-400">
-            {{ $t('components.guider.phd2.calibration') }}
-          </h3>
-          <Phd2CalibrationStep />
-        </div>
-
-        <!-- Guide Algorithm Settings -->
-        <div
-          class="p-2 sm:p-4 flex flex-col gap-2 sm:gap-3 bg-gray-800/50 rounded-lg border border-gray-700/50"
-        >
-          <h3 class="font-bold text-base text-cyan-400">
-            {{ $t('components.guider.phd2.guideAlgorithm') }}
-          </h3>
-          <Phd2GuideAlgorithmRA />
-          <Phd2GuideAlgorithmDEC />
-        </div>
-      </template>
-    </div>
+      <!-- Guide Algorithm Settings -->
+      <div
+        class="p-2 sm:p-4 flex flex-col gap-2 sm:gap-3 bg-gray-800/50 rounded-lg border border-gray-700/50"
+      >
+        <h3 class="font-bold text-base text-cyan-400">
+          {{ $t('components.guider.phd2.guideAlgorithm') }}
+        </h3>
+        <Phd2GuideAlgorithmRA />
+        <Phd2GuideAlgorithmDEC />
+      </div>
+    </template>
+  </div>
 </template>
 
 <script setup>

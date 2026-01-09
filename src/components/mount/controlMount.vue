7a566f3bba1ab472bdd664e264fbf2cc0ec3634c
Johannes <Johannes_Maier@gmx.de>
2026-01-09 09:54:09 +0100

    add reverse primary axis

--- src/components/mount/controlMount.vue
+++ src/components/mount/controlMount.vue
@@ -16,6 +16,17 @@
     <ButtonTrackingStop />
   </div>
 
+    <div class="flex mt-4 justify-center items-center">
+    <div class="w-3 mr-2 h-[1px] bg-gray-700"></div>
+    <p class="text-xs">{{ $t('components.mount.control.trackingMode') }}</p>
+    <div class="flex-grow ml-2 h-[1px] bg-gray-700"></div>
+  </div>
+  <div class="flex gap-x-2 mt-2">
+    <setReversePrimaryAxis />
+    
+  </div>
+
+
   <div class="flex mt-4 justify-center items-center">
     <div class="w-3 mr-2 h-[1px] bg-gray-700"></div>
     <p class="text-xs">{{ $t('components.mount.control.manuellControl') }}</p>
@@ -35,4 +46,5 @@ import ButtonPark from '@/components/mount/ButtonPark.vue';
 import ButtonSlewStop from '@/components/mount//ButtonSlewStop.vue';
 import ButtonTrackingMode from '@/components/mount/ButtonTrackingMode.vue';
 import ButtonTrackingStop from '@/components/mount/ButtonTrackingStop.vue';
+import setReversePrimaryAxis from './settings/setReversePrimaryAxis.vue';
 </script>

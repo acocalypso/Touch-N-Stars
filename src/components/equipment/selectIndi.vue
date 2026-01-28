6a345a60fde2ff28b7ea66caa91f5537e59cf324
Johannes <Johannes_Maier@gmx.de>
2026-01-28 11:06:51 +0100

    add loading spinner

--- src/components/equipment/selectIndi.vue
+++ src/components/equipment/selectIndi.vue
@@ -1,5 +1,14 @@
 <template>
   <div class="flex flex-col gap-1 w-full border border-gray-600 rounded-lg p-3">
+    <!-- Loading Spinner -->
+    <div v-if="loading" class="flex items-center justify-center py-4">
+      <svg class="animate-spin h-6 w-6 text-blue-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+        <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+        <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+      </svg>
+    </div>
+
+    <template v-else>
     <!-- Focuser -->
     <div class="flex flex-row w-full items-center">
       <label for="indi-focuser" class="mr-3 text-gray-200">
@@ -9,7 +18,7 @@
         id="indi-focuser"
         v-model="selectedFocuser"
         @change="onFocuserChange"
-        class="default-select min-w-40 ml-auto"
+        class="default-select w-40 ml-auto"
       >
         <option value="">None</option>
         <option v-for="item in focuser" :key="item.Name" :value="item.Name">
@@ -27,7 +36,7 @@
         id="indi-filterwheel"
         v-model="selectedFilterwheel"
         @change="onFilterwheelChange"
-        class="default-select min-w-40 ml-auto"
+        class="default-select w-40 ml-auto"
       >
         <option value="">None</option>
         <option v-for="item in filterwheel" :key="item.Name" :value="item.Name">
@@ -45,7 +54,7 @@
         id="indi-rotator"
         v-model="selectedRotator"
         @change="onRotatorChange"
-        class="default-select min-w-40 ml-auto"
+        class="default-select w-40 ml-auto"
       >
         <option value="">None</option>
         <option v-for="item in rotator" :key="item.Name" :value="item.Name">
@@ -63,7 +72,7 @@
         id="indi-telescope"
         v-model="selectedTelescope"
         @change="onTelescopeChange"
-        class="default-select min-w-40 ml-auto"
+        class="default-select w-40 ml-auto"
       >
         <option value="">None</option>
         <option v-for="item in telescope" :key="item.Name" :value="item.Name">
@@ -81,7 +90,7 @@
         id="indi-weather"
         v-model="selectedWeather"
         @change="onWeatherChange"
-        class="default-select min-w-40 ml-auto"
+        class="default-select w-40 ml-auto"
       >
         <option value="">None</option>
         <option v-for="item in weather" :key="item.Name" :value="item.Name">
@@ -99,7 +108,7 @@
         id="indi-switches"
         v-model="selectedSwitches"
         @change="onSwitchesChange"
-        class="default-select min-w-40 ml-auto"
+        class="default-select w-40 ml-auto"
       >
         <option value="">None</option>
         <option v-for="item in switches" :key="item.Name" :value="item.Name">
@@ -117,7 +126,7 @@
         id="indi-flatpanel"
         v-model="selectedFlatpanel"
         @change="onFlatpanelChange"
-        class="default-select min-w-40 ml-auto"
+        class="default-select w-40 ml-auto"
       >
         <option value="">None</option>
         <option v-for="item in flatpanel" :key="item.Name" :value="item.Name">
@@ -125,6 +134,7 @@
         </option>
       </select>
     </div>
+    </template>
   </div>
 </template>
 <script setup>
@@ -136,6 +146,7 @@ import { useEquipmentStore } from '@/store/equipmentStore';
 
 const store = apiStore();
 const equipmentStore = useEquipmentStore();
+const loading = ref(true);
 const focuser = ref([]);
 const filterwheel = ref([]);
 const rotator = ref([]);
@@ -274,6 +285,8 @@ onMounted(async () => {
     console.log('[SelectIndi] All INDI devices loaded');
   } catch (error) {
     console.error('[SelectIndi] Error fetching INDI devices:', error);
+  } finally {
+    loading.value = false;
   }
 });
 </script>

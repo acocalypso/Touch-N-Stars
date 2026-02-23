d83cea01e62fba0879f2d8796cca9c7fb058df4d
Johannes <Johannes_Maier@gmx.de>
2026-02-23 11:43:40 +0100

    fix eslint

--- src/plugins/pins/views/pins.vue
+++ src/plugins/pins/views/pins.vue
@@ -351,7 +351,7 @@
 </template>
 
 <script setup>
-import { ref, nextTick, onUnmounted, watch, onMounted } from 'vue';
+import { ref, nextTick, onUnmounted, watch } from 'vue';
 import { useI18n } from 'vue-i18n';
 import { useSettingsStore } from '@/store/settingsStore';
 import { usePinsStore } from '../store/pinsStore';

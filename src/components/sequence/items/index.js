bd5ae0f6fd4c9ac449f21fc7350bd5145a1b9e96
Johannes <Johannes_Maier@gmx.de>
2026-03-04 17:49:25 +0100

    SmartEx

--- src/components/sequence/items/index.js
+++ src/components/sequence/items/index.js
@@ -2,6 +2,7 @@ import TakeExposureItem from './TakeExposureItem.vue';
 import SwitchFilterItem from './SwitchFilterItem.vue';
 import LoopConditionItem from './LoopConditionItem.vue';
 import DitherAfterExposuresItem from './DitherAfterExposuresItem.vue';
+import SmartExposureItem from './SmartExposureItem.vue';
 import GenericItem from './GenericItem.vue';
 
 export const ITEM_COMPONENTS = {
@@ -9,6 +10,7 @@ export const ITEM_COMPONENTS = {
   'NINA.Sequencer.SequenceItem.FilterWheel.SwitchFilter': SwitchFilterItem,
   'NINA.Sequencer.Conditions.LoopCondition': LoopConditionItem,
   'NINA.Sequencer.Trigger.Guider.DitherAfterExposures': DitherAfterExposuresItem,
+  'NINA.Sequencer.SequenceItem.Imaging.SmartExposure': SmartExposureItem,
 };
 
 export { GenericItem };

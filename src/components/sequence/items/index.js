d50adfd8d1893cc426a5ea73b17db3da0908974c
Johannes <Johannes_Maier@gmx.de>
2026-03-05 20:11:35 +0000

    feat: add LoopWhileUnsafe and MoonIllumination condition components

--- src/components/sequence/items/index.js
+++ src/components/sequence/items/index.js
@@ -3,6 +3,7 @@ import SwitchFilterItem from './SwitchFilterItem.vue';
 import LoopConditionItem from './LoopConditionItem.vue';
 import LoopForTimeSpanItem from './LoopForTimeSpanItem.vue';
 import LoopWhileItem from './LoopWhileItem.vue';
+import LoopWhileUnsafeItem from './LoopWhileUnsafeItem.vue';
 import TimeConditionItem from './TimeConditionItem.vue';
 import DitherAfterExposuresItem from './DitherAfterExposuresItem.vue';
 import SmartExposureItem from './SmartExposureItem.vue';
@@ -12,6 +13,7 @@ import CenterItem from './CenterItem.vue';
 import DeepSkyObjectContainerItem from './DeepSkyObjectContainerItem.vue';
 import WarmCameraItem from './WarmCameraItem.vue';
 import MoonAltitudeConditionItem from './MoonAltitudeConditionItem.vue';
+import MoonIlluminationConditionItem from './MoonIlluminationConditionItem.vue';
 import AltitudeConditionItem from './AltitudeConditionItem.vue';
 import SunAltitudeConditionItem from './SunAltitudeConditionItem.vue';
 import AboveHorizonConditionItem from './AboveHorizonConditionItem.vue';
@@ -25,10 +27,13 @@ export const ITEM_COMPONENTS = {
   'NINA.Sequencer.Conditions.TimeSpanCondition': LoopForTimeSpanItem,
   'NINA.Sequencer.Conditions.TimeCondition': TimeConditionItem,
   'NINA.Sequencer.Conditions.LoopWhile': LoopWhileItem,
+  'NINA.Sequencer.Conditions.LoopWhileUnsafe': LoopWhileUnsafeItem,
   'NINA.Sequencer.Conditions.AltitudeCondition': AltitudeConditionItem,
   'NINA.Sequencer.Conditions.SunAltitudeCondition': SunAltitudeConditionItem,
   'NINA.Sequencer.Conditions.AboveHorizonCondition': AboveHorizonConditionItem,
   'NINA.Sequencer.Conditions.SafetyMonitorCondition': SafetyMonitorConditionItem,
+  'NINA.Sequencer.Conditions.MoonAltitudeCondition': MoonAltitudeConditionItem,
+  'NINA.Sequencer.Conditions.MoonIlluminationCondition': MoonIlluminationConditionItem,
   'NINA.Sequencer.Trigger.Guider.DitherAfterExposures': DitherAfterExposuresItem,
   'NINA.Sequencer.SequenceItem.Imaging.SmartExposure': SmartExposureItem,
   'NINA.Sequencer.SequenceItem.Autofocus.RunAutofocus': RunAutofocusItem,
@@ -40,7 +45,6 @@ export const ITEM_COMPONENTS = {
   'NINA.Sequencer.SequenceItem.Platesolving.Center': CenterItem,
   'NINA.Sequencer.Container.DeepSkyObjectContainer': DeepSkyObjectContainerItem,
   'NINA.Sequencer.SequenceItem.Camera.WarmCamera': WarmCameraItem,
-  'NINA.Sequencer.Conditions.MoonAltitudeCondition': MoonAltitudeConditionItem,
 };
 
 export { GenericItem };

613f6eb2678832160b5c8102ff26eca8de532b18
Johannes <Johannes_Maier@gmx.de>
2026-03-06 08:08:32 +0000

    feat: add SetSwitchValue sequence item component

--- src/components/sequence/items/index.js
+++ src/components/sequence/items/index.js
@@ -14,6 +14,7 @@ import PolarAlignmentItem from './PolarAlignmentItem.vue';
 import MoveRotatorMechanicalItem from './MoveRotatorMechanicalItem.vue';
 import SolveAndRotateItem from './SolveAndRotateItem.vue';
 import WaitUntilSafeItem from './WaitUntilSafeItem.vue';
+import SetSwitchValueItem from './SetSwitchValueItem.vue';
 import LoopConditionItem from './LoopConditionItem.vue';
 import LoopForTimeSpanItem from './LoopForTimeSpanItem.vue';
 import LoopWhileItem from './LoopWhileItem.vue';
@@ -107,6 +108,7 @@ export const ITEM_COMPONENTS = {
   'NINA.Sequencer.SequenceItem.Rotator.MoveRotatorMechanical': MoveRotatorMechanicalItem,
   'NINA.Sequencer.SequenceItem.Platesolving.SolveAndRotate': SolveAndRotateItem,
   'NINA.Sequencer.SequenceItem.SafetyMonitor.WaitUntilSafe': WaitUntilSafeItem,
+  'NINA.Sequencer.SequenceItem.Switch.SetSwitchValue': SetSwitchValueItem,
 };
 
 export { GenericItem };

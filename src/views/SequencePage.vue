c67efcef4652e01a3ab68ceb821746f2612395e3
Johannes <Johannes_Maier@gmx.de>
2026-03-10 08:05:53 +0000

    feat: add clear sequence

--- src/views/SequencePage.vue
+++ src/views/SequencePage.vue
@@ -1,5 +1,5 @@
 <template>
-  <SequenceV2Page v-if="store.isPINS" />
+  <SequenceV2Page v-if="!store.isPINS" />
 
   <div v-else class="min-h-screen bg-gradient-to-br from-gray-900 to-gray-800 p-4 md:p-6">
     <div class="fixed right-3 z-10" style="bottom: calc(env(safe-area-inset-bottom, 0px) + 48px)">

19769d37041fb6b692e257f93944ce23261373ec
Johannes <info@touch-n-stars.eu>
2026-05-31 20:34:17 +0200

    fix(number-picker): hide native browser spinner arrows in Firefox and WebKit

--- src/components/helpers/NumberInputPicker.vue
+++ src/components/helpers/NumberInputPicker.vue
@@ -206,3 +206,13 @@ function onDirectInput(event) {
   }
 }
 </script>
+
+<style scoped>
+input[type='number']::-webkit-inner-spin-button,
+input[type='number']::-webkit-outer-spin-button {
+  -webkit-appearance: none;
+}
+input[type='number'] {
+  -moz-appearance: textfield;
+}
+</style>
